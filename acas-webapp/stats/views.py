3c8de99b334e01918f6193fbff2cfa03b418f546
Invictus808 <BlaiseAranador@gmail.com>
2020-05-21 04:33:56 -1000

    Functioning 'Get OvS Information' and 'Configure' buttons.

--- acas-webapp/stats/views.py
+++ acas-webapp/stats/views.py
@@ -13,7 +13,10 @@ st = speedtest.Speedtest()
 # Description: Returns a string of the output of the executed command
 def execute(command):
 	command = command.split(' ')
-	result = subprocess.run(command, stdout=subprocess.PIPE).stdout.decode('utf-8')
+	output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	result = output.stdout.decode('utf-8')
+	if len(result) == 0:
+		result = output.stderr.decode('utf-8')
 	return result
 
 
@@ -64,35 +67,66 @@ def getActiveSlave(bondCommandOutput):
 	return active
 
 
+# Function: getConfigureActiveSlave(str, str)
+# Description: Returns a string of the success message of the configuration of the active slave
+def getConfigureActiveSlaveMessage(configureCommandOutput, port):
+	message = re.findall(r"(.*)\n", configureCommandOutput)
+	message = message[0]
+
+	if message == "done":
+		message = "Success: The active slave is now " + port + "."
+	elif message == "no change":
+		message = "Warning: Port " + port + " is already the active slave."
+	elif message == "no such slave":
+		message = "Error: There was no such port " + port + " used in the bond. Please select a port that is used in the bond."
+	elif message == "cannot make disabled slave active":
+		message = "Error: Port " + port + " is inactive. Please select an active port."
+	else:
+		message = "Error: Something went wrong. Try again or contact system admin to resolve persisting issues."
+
+	return message
+
+
 # GLOBAL VARIABLES
+uploadSpeedbps = None
+downloadSpeedbps = None
+uploadSpeedMbps = None
+downloadSpeedMbps = None
 bondPortStatus = None
 activeSlave = None
+configureActiveSlaveMessage = None
 
 
 # Function: statsView()
 # Description: Returns the components for the statistics page
 def statsView(request):
+	global uploadSpeedbps
+	global downloadSpeedbps
+	global uploadSpeedMbps
+	global downloadSpeedMbps
 	global bondPortStatus
 	global activeSlave
+	global configureActiveSlaveMessage
 
-	if request.method == "POST":
+	configureActiveSlaveMessage = None
+
+	if request.POST.get("run_speed_test"):
 		upload = getUploadSpeed()
 		download = getDownloadSpeed()
 		uploadSpeedbps = "{:.0f}".format(upload)
 		downloadSpeedbps = "{:.0f}".format(download)
 		uploadSpeedMbps = "{:.2f}".format(convert2Mega(upload))
 		downloadSpeedMbps = "{:.2f}".format(convert2Mega(download))
-		#bondPortStatus = None
-		#activeSlave = None
-	else:
-		command = 'sudo ovs-appctl bond/show bond0'
+	elif request.POST.get("get_ovs_information"):
+		command = "sudo ovs-appctl bond/show bond0"
 		commandResult = execute(command)
-		uploadSpeedbps = None
-		downloadSpeedbps = None
-		uploadSpeedMbps = None
-		downloadSpeedMbps = None
 		bondPortStatus = getBondPortStatus(commandResult)
 		activeSlave = getActiveSlave(commandResult)
+	elif request.POST.get("configure_active_slave"):
+		port = request.POST.get("port")
+		command = "sudo ovs-appctl bond/set-active-slave bond0 " + port
+		commandResult = execute(command)
+		configureActiveSlaveMessage = getConfigureActiveSlaveMessage(commandResult, port)
 
 	context = {
 		'uploadSpeedbps': uploadSpeedbps,
@@ -101,6 +135,7 @@ def statsView(request):
 		'downloadSpeedMbps': downloadSpeedMbps,
 		'bondPortStatus': bondPortStatus,
 		'activeSlave': activeSlave,
+		'configureActiveSlaveMessage': configureActiveSlaveMessage,
 	}
 
 	return render(request, "statistics.html", context)
